0bd6173f8390e118197c78a56e520b91d41c4f97
Bartosz <barzak200@gmail.com>
2020-12-22 16:55:13 +0100

    Downloading SpecialObjects and Paths

--- patients-transport-center/src/main/java/pl/group2/optimizer/impl/items/paths/Path.java
+++ patients-transport-center/src/main/java/pl/group2/optimizer/impl/items/paths/Path.java
@@ -1,14 +1,12 @@
 package pl.group2.optimizer.impl.items.paths;
 
-import pl.group2.optimizer.impl.items.Vertex;
-
 public class Path {
     private final int id;
-    private final Vertex from;
-    private final Vertex to;
+    private final int from;
+    private final int to;
     private final double distance;
 
-    public Path(int id, Vertex from, Vertex to, double distance) {
+    public Path(int id, int from, int to, double distance) {
         this.id = id;
         this.from = from;
         this.to = to;
