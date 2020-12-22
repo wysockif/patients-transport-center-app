45c3dc52f638d24b585c79b4e220df0147e8e185
wysockif <wysocki.f.00@gmail.com>
2020-12-22 15:48:36 +0100

    Implemented the class: Paths

--- patients-transport-center/src/main/java/pl/group2/optimizer/impl/items/paths/Paths.java
+++ patients-transport-center/src/main/java/pl/group2/optimizer/impl/items/paths/Paths.java
@@ -1,14 +1,28 @@
 package pl.group2.optimizer.impl.items.paths;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class Paths {
-    private final LinkedList<Path> paths;
+    private final List<Path> paths;
 
     public Paths() {
         paths = new LinkedList<>();
     }
 
+    public void addNewPath(Path path) {
+        checkIfArgumentIsNull(path);
+        paths.add(path);
+    }
 
+    public boolean contains(Path path){
+        checkIfArgumentIsNull(path);
+        return paths.contains(path);
+    }
 
+    private void checkIfArgumentIsNull(Object argument) {
+        if (argument == null) {
+            throw new IllegalArgumentException("Niezainicjowany argument");
+        }
+    }
 }
